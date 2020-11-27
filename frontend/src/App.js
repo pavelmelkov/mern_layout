f2ac02479fc147e677d35c260f6f803e5f45fdf7
Pavel <house.melkov@gmail.com>
2020-11-27 21:08:08 +0300

    layout commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import classes from './App.module.scss';
+
+console.log(classes.App)
+console.log(classes["App"])
+
+function App() {
+  return (
+    <div className={classes["App"]}>
+      <header className={classes["App-header"]}>
+        <img src={logo} className={classes["App-logo"]} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={classes["App-link"]}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
